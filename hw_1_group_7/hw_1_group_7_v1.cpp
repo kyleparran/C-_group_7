167feb804a5f7c4e373b05677efafa5456c45b9e
kyleparran <kyleparran@gmail.com>
2025-04-02 15:30:14 -0500

    Optimized function

--- hw_1_group_7/hw_1_group_7_v1.cpp
+++ hw_1_group_7/hw_1_group_7_v1.cpp
@@ -5,17 +5,14 @@
 
 const int SIZE = 4096;
 
-// Basic function to access matrix elements
-int getElement(const std::vector<std::vector<int>>& matrix, int row, int col) {
+inline int getElement(const std::vector<std::vector<int>>& matrix, int row, int col) {
     return matrix[row][col];
 }
 
-// Basic function to add two integers
-int add(int a, int b) {
+inline int add(int a, int b) {
     return a + b;
 }
 
-// Unoptimized summation function
 long long sumMatrixBasic(const std::vector<std::vector<int>>& matrix) {
     long long sum = 0;
     for (int i = 0; i < SIZE; ++i) {
@@ -26,15 +23,34 @@ long long sumMatrixBasic(const std::vector<std::vector<int>>& matrix) {
     return sum;
 }
 
+inline long long sumMatrixOptimized(const std::vector<int>& matrix) {
+    const int* ptr = matrix.data();
+    const int* end_ptr = ptr + SIZE * SIZE;
+    long long s1 = 0, s2 = 0, s3 = 0, s4 = 0;
+    while (ptr <= end_ptr - 4) {
+        s1 += ptr[0];
+        s2 += ptr[1];
+        s3 += ptr[2];
+        s4 += ptr[3];
+        ptr += 4;
+    }
+    while (ptr < end_ptr) {
+        s1 += *ptr++;
+    }
+    return s1 + s2 + s3 + s4;
+}
+
 int main() {
-    // Generate a large random matrix
     std::vector<std::vector<int>> matrix(SIZE, std::vector<int>(SIZE));
+    std::vector<int> flat_matrix(SIZE * SIZE);
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distrib(-100, 100);
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
-            matrix[i][j] = distrib(gen);
+            int value = distrib(gen);
+            matrix[i][j] = value;
+            flat_matrix[i * SIZE + j] = value;
         }
     }
 
@@ -46,9 +62,8 @@ int main() {
     std::cout << "Basic Sum: " << sum << std::endl;
     std::cout << "Basic Time: " << duration.count() << " milliseconds" << std::endl;
 
-    // Students will implement their optimized version here
     auto start_optimized = std::chrono::high_resolution_clock::now();
-    long long optimized_sum = 0; // Students will calculate this
+    long long optimized_sum = sumMatrixOptimized(flat_matrix);
     auto end_optimized = std::chrono::high_resolution_clock::now();
     auto duration_optimized = std::chrono::duration_cast<std::chrono::milliseconds>(end_optimized - start_optimized);
 
